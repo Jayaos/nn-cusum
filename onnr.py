c913809147cd68dcd5c37a0b14450efc3481df3d
Jay Lee <jayaos1002@gmail.com>
2026-03-22 18:49:30 -0400

    update

--- onnr.py
+++ onnr.py
@@ -8,22 +8,24 @@ import os
 import math
 
 
-def train_onnc_batch(batch_x, batch_y, model, loss_fn, optimizer, epoch, device):
+def onnr_loss(output_1, output_2, alpha):
+
+    return output_1.pow(2).mean() * ((1-alpha)/2) + output_2.pow(2).mean() * (alpha/2) - output_2.mean()
+
+    
+def train_onnr_batch(batch_x, batch_y, model, optimizer, epoch, alpha):
 
     assert batch_x.shape == batch_y.shape, print("batch_x and batch_y must have the same shape")
-    batch_size, dim = batch_x.shape
-
-    batch_data = torch.cat([batch_x,batch_y], axis=0).to(device)
-    labels_batch = torch.cat((torch.zeros(batch_size),torch.ones(batch_size)), axis=0).to(device)
 
     model.train()
     loss_sum = 0.
 
     for e in range(epoch):
         optimizer.zero_grad()
-        outputs = model(batch_data, torch.sigmoid) 
-        outputs = outputs.reshape(-1)
-        loss = loss_fn(outputs, labels_batch)
+        output_x = model(batch_x)
+        output_y = model(batch_y)
+
+        loss = onnr_loss(output_x, output_y, alpha)
         loss.backward()
         optimizer.step()
         loss_sum += loss.item()
@@ -31,33 +33,33 @@ def train_onnc_batch(batch_x, batch_y, model, loss_fn, optimizer, epoch, device)
     return loss_sum/epoch
 
 
-def test_onnc_batch(batch_x, batch_y, model, device):
+def test_onnr_batch(batch_x, batch_y, model_1, model_2):
 
     assert batch_x.shape == batch_y.shape, print("batch_x and batch_y must have the same shape")
-    batch_size, dim = batch_x.shape
-    eps = 1e-06
 
-    model.eval()
+    model_1.eval()
+    model_2.eval()
+
     with torch.no_grad():
         
-        output_x = model(batch_x.to(device), torch.sigmoid)
-        output_y = model(batch_y.to(device), torch.sigmoid)
-        output_x = torch.clip(output_x, min=eps, max=1-eps) # why clip here?
-        output_y = torch.clip(output_y, min=eps, max=1-eps) # why clip here?
-        kld_score = ((1-output_x) / output_x).log10().mean() + (output_y / (1-output_y)).log10().mean()
+        output_y_m1 = model_1(batch_y)
+        output_x_m2 = model_2(batch_x)
+        score = output_y_m1.mean() + output_x_m2.mean() - 2
 
-    return kld_score
+    return score
 
 
-def onnc_statistic(hidden_dims, pilot_X, arrival_Y, stride, window_size, learning_rate, epoch, device="cpu"):
+def onnr_statistic(hidden_dims, pilot_X, arrival_Y, alpha, stride, window_size, learning_rate, epoch, device="cpu"):
 
     maxT, dim = arrival_Y.shape
 
     # init model
-    model = MLP(dim, hidden_dims, 1).to(device)
+    model1 = MLP(dim, hidden_dims, 1).to(device)
+    model2 = MLP(dim, hidden_dims, 1).to(device)
 
     # optimization parameter
-    optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)    
+    optimizer1 = torch.optim.AdamW(model1.parameters(), lr=learning_rate)    
+    optimizer2 = torch.optim.AdamW(model2.parameters(), lr=learning_rate)    
 
     # placeholder for record
     wt = np.zeros(maxT)
@@ -80,21 +82,22 @@ def onnc_statistic(hidden_dims, pilot_X, arrival_Y, stride, window_size, learnin
             x_batch = pilot_X[np.mod(range(end - window_size, end), pilot_X.shape[0]), :]
             y_batch = arrival_Y[end - window_size:end]
 
-        x_batch = torch.Tensor(x_batch)
-        y_batch = torch.Tensor(y_batch)
-        onnc_stat = test_onnc_batch(x_batch, y_batch, model=model, device=device)
-        loss = train_onnc_batch(x_batch, y_batch, model, torch.nn.BCELoss(), optimizer, epoch, device=device)
+        x_batch = torch.tensor(x_batch).to(device)
+        y_batch = torch.tensor(y_batch).to(device)
+        onnr_stat = test_onnr_batch(x_batch, y_batch, model1, model2)
+        loss_1 = train_onnr_batch(x_batch, y_batch, model1, optimizer1, epoch, alpha)
+        loss_2 = train_onnr_batch(y_batch, x_batch, model2, optimizer2, epoch, alpha)
 
-        loss_record[end-1] = loss
-        wt[end-1] = onnc_stat
-
+        loss_record[end-1] = loss_1+loss_2
+        wt[end-1] = onnr_stat
+        
     return wt, loss_record
 
 
-def run_onnc(hidden_dims: list, window_size: int, stride: int, learning_rate: float, epoch: int,
+def run_onnr(hidden_dims: list, window_size: int, alpha: float, stride: int, learning_rate: float, epoch: int,
              f0_length: int, f1_length: int, burnin_length: int, 
              f0_sequence, f1_sequence, iter_num: int, save_dir: str, device: str):
-             
+    
     # reference sequence: (f0_length + f1_length) construced from f0_sequence
     # online sequence: (f0_length) construced from f0_sequence, (f1_length) construced from f1_sequence
     entire_sequence_len = f0_length + f1_length
@@ -122,14 +125,14 @@ def run_onnc(hidden_dims: list, window_size: int, stride: int, learning_rate: fl
         y = np.float32(np.concatenate([x_chunk[:f0_with_burnin_length], y_chunk]))
         x = np.float32(x_chunk[f0_with_burnin_length:])
 
-        Wt_h, loss_record = onnc_statistic(hidden_dims, x, y, stride, window_size, learning_rate, epoch, device)
+        Wt_h, loss_record = onnr_statistic(hidden_dims, x, y, alpha, stride, window_size, learning_rate, epoch, device)
         stat_record[i,:] = Wt_h[burnin_length:]
 
     print("saving results...")
     os.makedirs(save_dir, exist_ok=True)
     np.save(os.path.join(save_dir, 
-                         "onnc_iter{}_pre{}_post{}_b{}_d{}_l{}_s{}_w{}_e{}".format(iter_num, f0_length, f1_length, burnin_length, 
-                                                                          hidden_dims[0], len(hidden_dims),
-                                                                          stride, window_size, epoch)),
-                                                                          stat_record)
+                         "onnr_iter{}_pre{}_post{}_b{}_d{}_l{}_a{}_s{}_w{}_e{}".format(iter_num, f0_length, f1_length, burnin_length, 
+                                                                                   hidden_dims[0], len(hidden_dims), alpha,
+                                                                                   stride, window_size, epoch)),
+                                                                                   stat_record)
     
